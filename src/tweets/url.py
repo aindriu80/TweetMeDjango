7ccd87dbb7c75e62e9c15bb7e1b61261dc48b471
aindriu80 <aindriu80@gmail.com>
2019-04-17 21:28:03 +0100

    Dynamic URL Routing

--- src/tweets/url.py
+++ src/tweets/url.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
 # from .views import tweet_detail_view, tweet_list_view
-from .views import TweetListView, TweetDetailView
+from .views import TweetListView, TweetDetailView, tweet_detail_view
 
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
-    #url(r'^$', tweet_list_view, name='list'),
+    #url(r'^admin/', admin.site.urls),   
     url(r'^$', TweetListView.as_view(), name='list'),
-    #url(r'^1/$', tweet_detail_view, name='detail'),
-    url(r'^1/$', TweetDetailView.as_view(), name='detail'),
+    # url(r'^(?P<pk>\d+)/$', tweet_detail_view, name='detail'),     
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), #/tweet/1/
 ]
 
  
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 from .models import Tweet
 
@@ -19,10 +19,14 @@ from .models import Tweet
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
-    template_name = "tweets/detail_view.html"
+    # template_name = "tweets/detail_view.html"
 
-    def get_object(self):
-        return Tweet.objects.get(id=1)
+    # def get_object(self):
+    #     print(self.kwargs)
+    #     pk = self.kwargs.get("pk")
+    #     obj = get_object_or_404(Tweet, pk=pk)
+    #     # return Tweet.objects.get(id=pk)
+    #     return obj
     
 
 class TweetListView(ListView):
@@ -39,13 +43,14 @@ class TweetListView(ListView):
         return context
 
 
-# def tweet_detail_view(request, id=1):
-#     obj = Tweet.objects.get(id=id) # GET from database
-#     print(obj)
-#     context = {
-#         "object":obj
-#     }
-#     return render(request, "tweets/detail_view.html",context)
+def tweet_detail_view(request, pk=None): # pk == id
+    #obj = Tweet.objects.get(pk=pk) # GET from database
+    obj = get_object_or_404(Tweet, pk=pk)
+    print(obj)
+    context = {
+        "object":obj
+    }
+    return render(request, "tweets/detail_view.html",context)
 
 # Retrieve
 # def tweet_list_view(request):    
