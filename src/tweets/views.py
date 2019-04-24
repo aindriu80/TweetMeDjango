99eebc12213fbd9d26a7f676224960de2b328e6a
aindriu80 <aindriu80@gmail.com>
2019-04-24 21:01:12 +0100

    Advanced searching in Django

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import (
@@ -13,6 +14,7 @@ from django.views.generic import (
 from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
+
 
 # Create your views here.
 
@@ -47,8 +49,19 @@ class TweetDetailView(DetailView):
     
 
 class TweetListView(ListView):
-    template_name = "tweets/list_view.html"
-    queryset = Tweet.objects.all()
+    # template_name = "tweets/list_view.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+
+                )
+        return qs
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)      
