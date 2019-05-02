b7c918f7eedb4e96daa7f7c0eed9305a207227c5
aindriu80 <aindriu80@gmail.com>
2019-05-02 22:26:41 +0100

    API Serializer & View

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^tweet/', include(('tweets.url', 'tweet'),namespace='tweet')),
-    url(r'^api/tweet/', include(('tweets.api.url', 'tweet'),namespace='tweet-api')),
+    url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
 ]
 
 
