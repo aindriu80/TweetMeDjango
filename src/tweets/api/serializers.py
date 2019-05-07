7d3be34a3c1a6b4e465088efd500c8561155fed4
aindriu80 <aindriu80@gmail.com>
2019-05-07 21:34:35 +0100

    Generic API  - adding is authenticated

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -5,7 +5,7 @@ from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet # Or from ..models import Tweet
 
 class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer()
+    user = UserDisplaySerializer(read_only=True) # also write only
     class Meta:
         model = Tweet
         fields = [
