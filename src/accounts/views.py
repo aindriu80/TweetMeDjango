40f74aad107e2c64fe10a6223e8936775cafcd32
aindriu80 <aindriu80@gmail.com>
2019-05-13 21:19:55 +0100

    Creating a User detail page

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
-
+from django.views.generic import DetailView
 # Create your views here.
+
+User = get_user_model()
+
+class UserDetailView(DetailView):
+    template_name = 'accounts/user_detail.html'
+    queryset = User.objects.all()
