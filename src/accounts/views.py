4c3dae428534927beb4f7dd0fe41fcb98fd664d5
aindriu80 <aindriu80@gmail.com>
2019-05-13 21:33:28 +0100

    Lookup user based on username, slugfield

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 # Create your views here.
 
@@ -8,3 +8,12 @@ User = get_user_model()
 class UserDetailView(DetailView):
     template_name = 'accounts/user_detail.html'
     queryset = User.objects.all()
+    
+    # slug_field='username'    
+    # lookup_field = "username"
+    # def get_slug_field(self):
+        # return "username"
+        # slug_url_kwarg='username' 
+    
+    def get_object(self):
+        return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
--- src/tweets/models.py
+++ src/tweets/models.py
@@ -17,6 +17,9 @@ class Tweet(models.Model):
     
     def get_absolute_url(self):
         return reverse("tweet:detail", kwargs={"pk":self.pk})
+    
+    class Meta:
+        ordering = ['-timestamp']
 
     # def clean(self, *args, **kwargs):
     #     content = self.content
