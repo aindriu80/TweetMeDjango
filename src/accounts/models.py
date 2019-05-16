e34bfe33169bdf9581ae822b3dab22dc7de059d5
aindriu80 <aindriu80@gmail.com>
2019-05-16 20:47:05 +0100

    Getting rid of followed by users

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,10 +3,27 @@ from django.db import models
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+    def all(self):
+        qs = self.get_queryset().all()
+        print(dir(self))
+        print(self.instance) # user
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.PROTECT) # user.profile   
-    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')    # user.profile.following - all users following
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')    
+    # user.profile.following - all users following
     # user.followed_by -- users that follow me - reverse relationship
+
+    objects = UserProfileManager() # UserProfile.objects.all()
+    # abc = UserProfileManager() # UserProfile.abc.all()
 
     def __str__(self):
         return str(self.following.all().count())
+          
