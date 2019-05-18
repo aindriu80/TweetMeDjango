17c5321b62fbd3cada2c7b00f8a02da3bab6b30f
aindriu80 <aindriu80@gmail.com>
2019-05-18 21:19:52 +0100

    Model Manager for Follow Toggle

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -15,6 +15,25 @@ class UserProfileManager(models.Manager):
             pass
         return qs
 
+    def toggle_follow(self, user, to_toggle_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user) # (user_obj, true)
+        if to_toggle_user in user_profile.following.all():
+            user_profile.following.remove(to_toggle_user)
+            added = False
+        else:
+            user_profile.following.add(to_toggle_user)
+            added = True
+        return added
+    
+    def is_following(self, user, followed_by_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user)  
+        if created:
+            return False
+        if followed_by_user in user_profile.following.all():
+            return True
+        return False
+        
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.PROTECT) # user.profile   
     following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')    
