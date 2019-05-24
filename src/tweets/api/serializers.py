52083d61855287dc583a431becbe5d01af9b680a
aindriu80 <aindriu80@gmail.com>
2019-05-24 21:55:49 +0100

    Retweeting from the parent

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -5,11 +5,11 @@ from rest_framework import serializers
 from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet # Or from ..models import Tweet
 
-class TweetModelSerializer(serializers.ModelSerializer):
+class ParentTweetModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer(read_only=True) # also write only
     date_display = serializers.SerializerMethodField()
     timesince= serializers.SerializerMethodField()
-    is_retweet = serializers.SerializerMethodField()
+    
     class Meta:
         model = Tweet
         fields = [
@@ -19,7 +19,31 @@ class TweetModelSerializer(serializers.ModelSerializer):
             'timestamp',
             'date_display',
             'timesince',
-            'is_retweet',
+     
+        ]
+
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%d %b, %Y at %I:%M %p") # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp)+ " ago"
+
+class TweetModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer(read_only=True) # also write only
+    date_display = serializers.SerializerMethodField()
+    timesince= serializers.SerializerMethodField()
+    parent = ParentTweetModelSerializer()
+
+    class Meta:
+        model = Tweet
+        fields = [
+            'id',
+            'user',
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince',
+            'parent',
         ]
 
     def get_is_retweet(self, obj):
@@ -32,4 +56,4 @@ class TweetModelSerializer(serializers.ModelSerializer):
         return obj.timestamp.strftime("%d %b, %Y at %I:%M %p") # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     
     def get_timesince(self, obj):
-        return timesince(obj.timestamp)+ " ago"
+        return timesince(obj.timestamp)+ " ago"        
