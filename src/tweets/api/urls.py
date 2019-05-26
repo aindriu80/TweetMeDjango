c6f767126b7b412bb8a49e4d17e52ef5721b6ba4
aindriu80 <aindriu80@gmail.com>
2019-05-26 16:00:20 +0100

    JQuery - ensuring only 1 retweet of a message per day

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (        
+    RetweetAPIView,
     TweetCreateAPIView,
-    TweetListAPIView
+    TweetListAPIView,
     )
 
 
@@ -12,6 +13,7 @@ urlpatterns = [
     #url(r'^$', RedirectView.as_view(url="/")), #/tweet/
     url(r'^$', TweetListAPIView.as_view(), name='list'), # api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # api/tweet/
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), #
     #url(r'^create/$', TweetCreateView.as_view(), name='create'), #/tweet/create
     #url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), #/tweet/1/
     #url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), #/tweet/1/
