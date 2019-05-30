623af7c7325205c86a1ebe453948d683fce943e9
aindriu80 <aindriu80@gmail.com>
2019-05-30 21:28:14 +0100

    Refactored

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -36,27 +36,20 @@ class TweetListAPIView(generics.ListAPIView):
     
     def get_queryset(self, *args, **kwargs):
         requested_user = self.kwargs.get("username")
-        if requested_user:
-            qs = Tweet.objects.filter(user__username=requested_user).order_by("-timestamp")
-            query = self.request.GET.get("q",None)
-            if query is not None:
-                qs = qs.filter(
-                    Q(content__icontains=query) |
-                    Q(user__username__icontains=query)
-                    )
-            return qs
 
+        if requested_user:
+            qs = Tweet.objects.filter(user__username=requested_user).order_by("-timestamp")            
         else:
-            im_following = my_follow = self.request.user.profile.get_following() # none
+            im_following = self.request.user.profile.get_following() # none
             qs1 = Tweet.objects.filter(user__in=im_following).order_by("-timestamp")
             qs2 = Tweet.objects.filter(user=self.request.user)
             qs = (qs1 | qs2).distinct().order_by("-timestamp")
-            print(self.request.GET)
-            query = self.request.GET.get("q", None)
-            if query is not None:
-                qs = qs.filter(
-                    Q(content__icontains=query) |
-                    Q(user__username__icontains=query)
-                    )
-            return qs
+
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+                )
+        return qs
 
