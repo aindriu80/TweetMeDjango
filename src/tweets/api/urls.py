6dd1489e3a375e2f4d6a97327f37b0db4c244e9d
aindriu80 <aindriu80@gmail.com>
2019-05-31 21:56:19 +0100

    Like the tweet

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (        
+    LikeToggleAPIView,
     RetweetAPIView,
     TweetCreateAPIView,
     TweetListAPIView,
@@ -13,6 +14,7 @@ urlpatterns = [
     #url(r'^$', RedirectView.as_view(url="/")), #/tweet/
     url(r'^$', TweetListAPIView.as_view(), name='list'), # api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # api/tweet/
+    url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'), # api/tweet/id/tweet/
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), # api/tweet/id/tweet/
     #url(r'^create/$', TweetCreateView.as_view(), name='create'), #/tweet/create
     #url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), #/tweet/1/
